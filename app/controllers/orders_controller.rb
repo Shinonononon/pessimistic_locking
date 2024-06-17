a4e46b6ae073aa82a90a2ba6b6d7741cbd689553
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-17 18:08:12 +0900

    test9

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,6 @@ class OrdersController < ApplicationController
   def create
     ActiveRecord::Base.transaction do
       @order = current_user.orders.build(order_params)
-      @item = Item.lock.find_by(id: line_item.item_id)
       @order.save!
       @order.update_total_quantity
     end
